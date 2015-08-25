1c82b7991e97d4f5e5bf67e6831b1119c395e09c
kevpl <kevin.imber@puppetlabs.com>
2015-08-25 09:13:10 -0700

    added acceptance testing

---
+++ acceptance/tests/first.rb
@@ -0,0 +1,8 @@
+
+# Acceptance level testing goes into files in the tests directory like this one,
+# Each file corresponding to a new test made up of individual testing steps
+test_name "Template Acceptance Test Example"
+
+step "Fail fast!"
+
+fail_test("There are no acceptance tests yet!")
