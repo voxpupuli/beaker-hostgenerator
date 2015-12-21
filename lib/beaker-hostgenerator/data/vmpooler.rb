c7cfbaa0f69d3940c02ce21d44ccfb938fb013f6
LuvCurves <stan@puppetlabs.com>
2015-12-21 14:24:06 -0800

    (QENG-3308)(QENG-3309) Add support for Cisco platforms

--- lib/beaker-hostgenerator/data/vmpooler.rb
+++ lib/beaker-hostgenerator/data/vmpooler.rb
@@ -35,6 +35,14 @@ module BeakerHostGenerator
           'platform' => 'el-7-x86_64',
           'template' => 'centos-7-x86_64'
         },
+        'cisco5-64' => {
+          'platform' => 'cisconxos-9-x86_64',
+          'template' => 'cisco-nxos-9k-x86_64'
+        },
+        'cisco7-64' => {
+          'platform' => 'ciscoexr-9-x86_64',
+          'template' => 'cisco-exr-9k-x86_64'
+        },
         'debian6-32' => {
           'platform' => 'debian-6-i386',
           'template' => 'debian-6-i386'
