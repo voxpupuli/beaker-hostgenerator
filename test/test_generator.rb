388867d167c013894d8f707fc9118d561a53d586
Wayne <wayne@puppetlabs.com>
2016-01-29 15:27:08 -0800

    (QENG-3770) Add GeneratorTestHelpers and default options test for test class.

--- test/test_generator.rb
+++ test/test_generator.rb
@@ -1,13 +1,37 @@
 require "minitest/autorun"
+require "yaml"
+
+module GeneratorTestHelpers
+  def run_cli_with_options options = []
+    # Calls beaker-hostgenerator with the given options, then reads the
+    # resulting yaml stdout, then returns a hash of that data structure to be
+    # asserted upon.
+    options.insert(0, "beaker-hostgenerator")
+    command = options.join(" ")
+    stdin, stdout, stderr = Open3.popen3(String(command))
+    hash = YAML.load(stdout.read())
+    return hash
+  end
+end
 
 class TestGenerator < Minitest::Test
+  include GeneratorTestHelpers
+
   def setup
-    @default_options = {
-      list_platforms_and_roles: false,
-      disable_default_role: false,
-      disable_role_config: false,
-      hypervisor: 'vmpooler',
-    }
+    @default_options = [
+      'centos6-64mdca',
+    ]
+  end
+
+  def test_default_options
+    hash = run_cli_with_options(@default_options)
+    hosts = hash['HOSTS']
+
+    assert_includes(hosts, "centos6-64-1")
+    centos6 = hosts['centos6-64-1']
+    ["master", "database", "dashboard", "agent"].each do |role| 
+      assert_includes(centos6['roles'], role)
+    end
   end
 
   def test_pe_dir_option
