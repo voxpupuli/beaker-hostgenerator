f4fd1cf5a9630a545d8cec22e3d7fc213c0977ce
Wayne Warren <wayne@puppetlabs.com>
2016-01-30 17:53:16 -0800

    (QENG-3370) Move helper methods into helper module.
    Unclutters the fixture generator class and maintains distinction between test
    and helper methods.

--- test/util/generate_fixtures.rb
+++ test/util/generate_fixtures.rb
@@ -1,52 +1,14 @@
-require "yaml"
-
 require "minitest/autorun"
 
 require 'beaker-hostgenerator'
-require 'beaker-hostgenerator/data/vmpooler'
-require 'beaker-hostgenerator/data'
 
 require 'util/generator_helpers'
 
 class GenerateFixtures < Minitest::Test
   include GeneratorTestHelpers
-  include BeakerHostGenerator::Data
-  include BeakerHostGenerator::Data::Vmpooler
 
   def setup
     @fixture_root = 'test/fixtures/'
-  end
-
-  def open_file path
-    dirname = File.join(path[1, path.length - 2]) # wtf
-    filename = File.join(path)
-    FileUtils.mkdir_p(dirname)
-    return File.open(filename, "w")
-  end
-
-  def generate_fixture(dirname, options, spec)
-    specopts = options + [spec]
-    options_string = specopts.join(" ")
-    generated_hash = run_cli_with_options(specopts)
-
-    fixture_hash = {
-      "options_string" => options_string,
-      "environment_variables" => {},
-      "expected_hash" => generated_hash,
-      "expected_exception" => nil,
-    }
-    fixture_yaml = fixture_hash.to_yaml
-
-    fixture_file = open_file([Dir.pwd, @fixture_root] + dirname + [spec])
-    fixture_file.write(fixture_yaml)
-  end
-
-  def generate_fixtures_using_osinfo(dirname, options=[], bhg_version=0)
-    osinfo = get_osinfo(bhg_version)
-    osinfo.each_key do |platform_info|
-      spec = "#{platform_info}aulcdfm"
-      generate_fixture(dirname, options, spec)
-    end
   end
 
   def test_generate_default_fixtures
--- test/util/generator_helpers.rb
+++ test/util/generator_helpers.rb
@@ -1,8 +1,13 @@
 require "yaml"
 
 require 'beaker-hostgenerator'
+require 'beaker-hostgenerator/data/vmpooler'
+require 'beaker-hostgenerator/data'
 
 module GeneratorTestHelpers
+  include BeakerHostGenerator::Data
+  include BeakerHostGenerator::Data::Vmpooler
+
   def run_cli_with_options options = []
     STDERR.reopen("stderr.txt", "w")
     cli = BeakerHostGenerator::CLI.new(options)
@@ -11,4 +16,43 @@ module GeneratorTestHelpers
     hash = YAML.load(yaml_string)
     return hash
   end
+
+  def open_file path
+    # Takes a list of path elements.
+    # Returns a file object where the basename of the file is the last path
+    # element.
+    dirname = File.join(path[1, path.length - 2]) # wtf
+    filename = File.join(path)
+    FileUtils.mkdir_p(dirname)
+    return File.open(filename, "w")
+  end
+
+  def generate_fixture(relative_path, options, spec)
+    specopts = options + [spec]
+    options_string = specopts.join(" ")
+    generated_hash = run_cli_with_options(specopts)
+
+    fixture_hash = {
+      "options_string" => options_string,
+      "environment_variables" => {},
+      "expected_hash" => generated_hash,
+      "expected_exception" => nil,
+    }
+    fixture_yaml = fixture_hash.to_yaml
+
+    fixture_file = open_file([Dir.pwd, @fixture_root] + relative_path + [spec])
+    fixture_file.write(fixture_yaml)
+  end
+
+  def generate_fixtures_using_osinfo(relative_path,
+                                     options=[],
+                                     bhg_version=0,
+                                     )
+    osinfo = get_osinfo(bhg_version)
+    osinfo.each_key do |platform_info|
+      spec = "#{platform_info}aulcdfm"
+      generate_fixture(relative_path, options, spec)
+    end
+  end
+
 end
