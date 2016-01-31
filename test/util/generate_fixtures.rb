71e4efcab96228d7b4ea0210b2a248650b8a7ff7
Wayne Warren <wayne@puppetlabs.com>
2016-01-31 01:32:22 -0800

    (QENG-3370) Generate various option fixtures.

--- test/util/generate_fixtures.rb
+++ test/util/generate_fixtures.rb
@@ -12,13 +12,11 @@ class GenerateFixtures < Minitest::Test
     @simple_roles = ["a", "u", "l", "c", "d", "f", "m", "aulcdfm"]
   end
 
-  def test_generate_default_fixtures
+  def test_generate_fixtures
     # Validates single-host scenarios using all short-form role aliases with no
-    # optional flags.
-    generate_fixtures_using_osinfo(["default"], @simple_roles.cycle)
-  end
+    # optional flags'
+    generate_fixtures_using_osinfo(['default'], @simple_roles.cycle, [])
 
-  def test_generate_versioned_fixtures
     # Validates single-host scenarios using all short-form role aliases with the
     # addition of the --osinfo-version flag to indicate which BHG version to
     # generate host configs for.
@@ -28,8 +26,33 @@ class GenerateFixtures < Minitest::Test
                                      ["--osinfo-version", "#{bhg_version}"],
                                      bhg_version)
     end
-  end
 
-  def test_generate_pe_dir_fixtures
+    # Validates the use of the pe ver/dir type options.
+    [
+      {
+        'path' => ['pe_upgrade_ver'],
+        'options' => ['--pe_upgrade_ver', '2020.7.3'],
+        'spec' => 'centos6-64mdc'
+        },
+      {
+        'path' => ['pe_ver'],
+        'options' => ['--pe_ver', '2020.7'],
+        'spec' => 'centos6-64mdc'
+        },
+      {
+        'path' => ['pe_upgrade_dir'],
+        'options' => ['--pe_upgrade_dir', '/opt/whatever'],
+        'spec' => 'centos6-64mdc'
+        },
+      {
+        'path' => ['pe_dir'],
+        'options' => ['--pe_dir', '/opt/hello'],
+        'spec' => 'centos6-64mdc'
+        }
+      ].each do |fixture_info|
+      generate_fixture(fixture_info['path'],
+                       fixture_info['options'],
+                       fixture_info['spec'])
+    end
   end
 end
