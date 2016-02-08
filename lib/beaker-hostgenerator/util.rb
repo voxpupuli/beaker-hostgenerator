voxpupuli/beaker-hostgenerator

commit eecc04c882c972f5e4457736c9315c7d7bbae25b

(QENG-3370) Use mixins instead of module method references.

lib/beaker-hostgenerator/util.rb
@@ -22,7 +22,7 @@ module BeakerHostGenerator
 
     def fixup_node(cfg)
       # PE 2.8 doesn't exist for EL 4. We use 2.0 instead.
-      if cfg['platform'] =~ /el-4/ and BeakerHostGenerator::Data.pe_version =~ /2\.8/
+      if cfg['platform'] =~ /el-4/ and pe_version =~ /2\.8/
         cfg['pe_ver'] = "2.0.3"
       end
     end
@@ -58,7 +58,5 @@ module BeakerHostGenerator
     def get_roles
       return BeakerHostGenerator::Data::ROLES
     end
-
-    module_function :dump_hosts, :get_platforms, :get_roles, :pe_dir
   end
 end

lib/beaker-hostgenerator/data.rb
@@ -61,7 +61,5 @@ module BeakerHostGenerator
         'consoleport' => 443,
       }
     }
-
-    module_function :pe_version, :pe_family, :pe_upgrade_version, :pe_upgrade_family
   end
 end

lib/beaker-hostgenerator/generator.rb
@@ -51,14 +51,10 @@ module BeakerHostGenerator
         node_info['nodeid'] = nodeid[ostype]
 
         config = {
-          'pe_dir' => BeakerHostGenerator::Utils.pe_dir(
-            BeakerHostGenerator::Data.pe_version,
-            BeakerHostGenerator::Data.pe_family),
-          'pe_ver' => BeakerHostGenerator::Data.pe_version,
-          'pe_upgrade_dir' => BeakerHostGenerator::Utils.pe_dir(
-            BeakerHostGenerator::Data.pe_upgrade_version,
-            BeakerHostGenerator::Data.pe_upgrade_family),
-          'pe_upgrade_ver' => BeakerHostGenerator::Data.pe_upgrade_version,
+          'pe_dir' => pe_dir(pe_version, pe_family),
+          'pe_ver' => pe_version,
+          'pe_upgrade_dir' => pe_dir(pe_upgrade_version, pe_upgrade_family),
+          'pe_upgrade_ver' => pe_upgrade_version,
         }
 
         [:pe_dir, :pe_ver, :pe_upgrade_dir, :pe_upgrade_ver].each do |option|

commit 865b04c27c44a7ba58660b0db5cb71155c0685a6

(MAINT) Fix bug introduced by eecc04c

lib/beaker-hostgenerator/util.rb
@@ -58,5 +58,7 @@ module BeakerHostGenerator
     def get_roles
       return BeakerHostGenerator::Data::ROLES
     end
+
+    module_function :get_roles, :get_platforms, :dump_hosts, :fixup_node, :pe_dir
   end
 end