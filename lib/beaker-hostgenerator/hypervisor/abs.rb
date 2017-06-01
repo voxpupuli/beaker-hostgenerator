a9ddf44bf8dda5eadb8bde5d0522b0ae47afd4c8
Nate Wolfe <nwolfe@puppet.com>
2017-05-31 17:48:55 -0700

    (QENG-4965) Add ABS support for hardware platforms
    This commit makes the hardware platforms compatible with the ABS hypervisor and
    the --templates-only switch to enable hardware support in Jenkins via ABS.

--- lib/beaker-hostgenerator/hypervisor/abs.rb
+++ lib/beaker-hostgenerator/hypervisor/abs.rb
@@ -10,19 +10,24 @@ module BeakerHostGenerator
     # determine the type of platform that's being requested.
     #
     # Therefore, this hypervisor just reuses the vmpooler hypervisor template
-    # values, and in the future will likely contain the fake template values to
-    # use for non-vmpooler platforms like AIX.
+    # values, in addition to ABS-only values for hardware platforms.
     class ABS < BeakerHostGenerator::Hypervisor::Interface
       include BeakerHostGenerator::Data
 
       def generate_node(node_info, base_config, bhg_version)
         base_config['hypervisor'] = 'abs'
 
-        # For now, we just reuse the vmpooler data
+        # Grab vmpooler data for this platform and any hardware (ABS) data.
+        # The assumption here is that these are mutually exclusive; that is,
+        # any given platform will have *either* :vmpooler data or :abs data
+        # so we're not worried about one overriding the other when we merge
+        # the hashes together.
         platform = node_info['platform']
-        platform_info = get_platform_info(bhg_version, platform, :vmpooler)
+        vmpooler_platform_info = get_platform_info(bhg_version, platform, :vmpooler)
+        abs_platform_info = get_platform_info(bhg_version, platform, :abs)
 
-        base_config.deep_merge! platform_info
+        base_config.deep_merge! vmpooler_platform_info
+        base_config.deep_merge! abs_platform_info
 
         return base_config
       end
--- spec/beaker-hostgenerator/abs-support_spec.rb
+++ spec/beaker-hostgenerator/abs-support_spec.rb
@@ -5,9 +5,18 @@ module BeakerHostGenerator
   describe AbsSupport do
     describe 'extract_templates' do
       it 'Returns a JSON map with template counts' do
-        input = ['--templates-only', 'centos6-64m-centos6-64m-redhat7-64m']
+        input = ['aix53-POWER-aix61-POWER-aix71-POWER-aix72-POWER-huaweios6-POWER-solaris10-SPARC-solaris11-SPARC-centos7-64',
+                 '--templates-only',
+                 '--hypervisor', 'abs']
         expect( JSON.load(BeakerHostGenerator::CLI.new(input).execute) ).
-          to eq({"centos-6-x86_64" => 2, "redhat-7-x86_64" => 1})
+          to eq({'aix-5.3-power' => 1,
+                 'aix-6.1-power' => 1,
+                 'aix-7.1-power' => 1,
+                 'aix-7.2-power' => 1,
+                 'huaweios-6-powerpc' => 1,
+                 'solaris-10-sparc' => 1,
+                 'solaris-11-sparc' => 1,
+                 'centos-7-x86_64' => 1})
       end
     end
   end
