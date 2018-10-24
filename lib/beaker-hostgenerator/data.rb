1d36003bbba467ceeac12cd364e323a8c5dd2737
Mickaël Canévet <mickael.canevet@camptocamp.com>
2018-10-24 11:06:49 +0200

    Add gnupg to ubuntu1804-64 to prevent "Could not find a suitable provider for apt_key"

--- lib/beaker-hostgenerator/data.rb
+++ lib/beaker-hostgenerator/data.rb
@@ -1103,7 +1103,7 @@ module BeakerHostGenerator
           :docker => {
             'docker_image_commands' => [
               'systemctl mask getty@tty1.service getty-static.service',
-              'apt-get install -y net-tools wget locales apt-transport-https iproute2',
+              'apt-get install -y net-tools wget locales apt-transport-https iproute2 gnupg',
               'locale-gen en_US.UTF-8',
               'echo LANG=en_US.UTF-8 > /etc/default/locale'
             ]
