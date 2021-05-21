209336f52bf76fd103f86793c2b122d1884e580e
Gene Liverman <gene.liverman+14713432@puppet.com>
2021-05-21 16:04:51 -0400

    Ensure amd64 containers are pulled when needed
    Prior to this, asking for a `centos8-64` host while running Beaker on a
    machine with an ARM processor such the M1 baesd Macs would pull the ARM
    version of CentOS instead of the x86-64 based one.

--- lib/beaker-hostgenerator/hypervisor/docker.rb
+++ lib/beaker-hostgenerator/hypervisor/docker.rb
@@ -12,6 +12,9 @@ module BeakerHostGenerator
         base_config['image'] = node_info['ostype'].sub(/(\d)/, ':\1')
         base_config['image'].sub!(/(\w+)/, '\1/leap') if node_info['ostype'] =~ /^opensuse/
         base_config['image'].sub!(/(\d{2})/, '\1.') if node_info['ostype'] =~ /^ubuntu/
+        if node_info['bits'] == '64'
+          base_config['image'] = "amd64/#{base_config['image']}"
+        end
 
         return base_generate_node(node_info, base_config, bhg_version, :docker)
       end
