voxpupuli/beaker-hostgenerator

commit a4bdd3b3c51ccc65d6e75a9fa245a2f240b0d12d

Use CentOS Stream 8 instead of CentOS Linux 8
CentOS Linux 8 went End Of Life at the end of 2021 and at the end of
January it will disappear from the mirrors. Stream is the supported
replacement. By using the Vagrant box and container image it should mean
most testing will not notice the difference.

lib/beaker-hostgenerator/data.rb
@@ -297,7 +297,11 @@ module BeakerHostGenerator
           :general => {
             'platform' => 'el-8-x86_64'
           },
+          :vagrant => {
+            'box' => 'centos/stream8',
+          },
           :docker => {
+            'image'                 => 'quay.io/centos/centos:stream8',
             'docker_image_commands' => [
               'cp /bin/true /sbin/agetty',
               'yum install -y crontabs initscripts iproute openssl wget which glibc-langpack-en'

commit de9a0ef1c9a577087ed494b1824095d0612e1070

Install hostname on CentOS 8 Stream in docker
This is not available in the minimal CentOS 8 Stream containers and
specinfra relies on this for the hostname -f command to determine the
FQDN.

Fixes: a4bdd3b3c51ccc65d6e75a9fa245a2f240b0d12d

lib/beaker-hostgenerator/data.rb
@@ -304,7 +304,7 @@ module BeakerHostGenerator
             'image'                 => 'quay.io/centos/centos:stream8',
             'docker_image_commands' => [
               'cp /bin/true /sbin/agetty',
-              'yum install -y crontabs initscripts iproute openssl wget which glibc-langpack-en'
+              'yum install -y crontabs initscripts iproute openssl wget which glibc-langpack-en hostname'
             ]
           }
         },