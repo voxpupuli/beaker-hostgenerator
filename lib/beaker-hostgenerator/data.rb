68e808db5f7e3396b66ce7bc9c6bfd25faeb1c0a
Tim Meusel <tim@bastelfreak.de>
2023-02-23 08:43:28 +0100

    rubocop: fix Layout/CaseIndentation

--- lib/beaker-hostgenerator/data.rb
+++ lib/beaker-hostgenerator/data.rb
@@ -30,16 +30,16 @@ module BeakerHostGenerator
 
       base_regex = '(\A\d+\.\d+)\.\d+'
       source = case version
-      when /#{base_regex}\Z/
+               when /#{base_regex}\Z/
         then "#{PE_TARBALL_SERVER}/archives/releases/#{version}/"
-      when /#{base_regex}-rc\d+\Z/
+               when /#{base_regex}-rc\d+\Z/
         then "#{PE_TARBALL_SERVER}/archives/internal/%s/"
-      when /#{base_regex}-.*(PEZ|pez)_.*/
+               when /#{base_regex}-.*(PEZ|pez)_.*/
         then "#{PE_TARBALL_SERVER}/%s/feature/ci-ready"
-      when /#{base_regex}-.*/
+               when /#{base_regex}-.*/
         then "#{PE_TARBALL_SERVER}/%s/ci-ready"
-      else
-        ''
+               else
+                 ''
       end
 
       pe_family = $1
