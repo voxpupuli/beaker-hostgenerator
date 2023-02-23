d352c4b3d51802d2153263c1f20c09978d1ab915
Tim Meusel <tim@bastelfreak.de>
2023-02-23 08:48:36 +0100

    rubocop: fix Layout/HeredocIndentation

--- lib/beaker-hostgenerator/cli.rb
+++ lib/beaker-hostgenerator/cli.rb
@@ -24,50 +24,50 @@ module BeakerHostGenerator
       argv.push('--help') if argv.empty?
 
       optparse = OptionParser.new do |opts|
-        opts.banner = <<-eos
-Usage: beaker-hostgenerator [options] <layout>
+        opts.banner = <<~eos
+          Usage: beaker-hostgenerator [options] <layout>
 
- where <layout> takes the following form:
-  <platform>-<arch>[[<arbitrary-roles>,[...]].]<roles>[{<arbitrary-settings>,[...]}][-<arch>[[<arbitrary-roles>,[...]].]<roles>[{<arbitrary-settings>,[...]}]][-<layout>]
+           where <layout> takes the following form:
+            <platform>-<arch>[[<arbitrary-roles>,[...]].]<roles>[{<arbitrary-settings>,[...]}][-<arch>[[<arbitrary-roles>,[...]].]<roles>[{<arbitrary-settings>,[...]}]][-<layout>]
 
- examples:
-  centos9-64mdca-64a
-   1 CentOS 9 64 bit node with roles = master, database, agent, dashboard
-   1 CentOS 9 64 bit node with roles = agent
+           examples:
+            centos9-64mdca-64a
+             1 CentOS 9 64 bit node with roles = master, database, agent, dashboard
+             1 CentOS 9 64 bit node with roles = agent
 
-  debian11-64m-64ad-64ac-centos9-64a
-   1 Debian 11 64 bit node with roles = master
-   1 Debian 11 64 bit node with roles = agent, database
-   1 Debian 11 64 bit node with roles = agent, dashboard
-   1 CentOS 9 64 bit node with roles = agent
+            debian11-64m-64ad-64ac-centos9-64a
+             1 Debian 11 64 bit node with roles = master
+             1 Debian 11 64 bit node with roles = agent, database
+             1 Debian 11 64 bit node with roles = agent, dashboard
+             1 CentOS 9 64 bit node with roles = agent
 
-  debian11-64m-windows2019_core-64a
-   1 Debian 11 64 bit node with roles = master
-   1 Windows 2019 64 bit node with roles = agent
+            debian11-64m-windows2019_core-64a
+             1 Debian 11 64 bit node with roles = master
+             1 Windows 2019 64 bit node with roles = agent
 
- example with arbitrary roles:
-  centos9-64compile_master,another_role.ma
-   1 CentOS 9 64 bit node with roles = master, agent, compile_master, another_role
+           example with arbitrary roles:
+            centos9-64compile_master,another_role.ma
+             1 CentOS 9 64 bit node with roles = master, agent, compile_master, another_role
 
- example with arbitrary host settings:
-  centos9-64m{hypervisor=none\\,hostname=static1\\,my-key=my-value}-64a
-   1 CentOS 9 64 bit node with roles = master, hypervisor = none, node name = static1, and my-key = my-value
-   1 CentOS 9 64 bit node with roles = agent and the default hypervisor
+           example with arbitrary host settings:
+            centos9-64m{hypervisor=none\\,hostname=static1\\,my-key=my-value}-64a
+             1 CentOS 9 64 bit node with roles = master, hypervisor = none, node name = static1, and my-key = my-value
+             1 CentOS 9 64 bit node with roles = agent and the default hypervisor
 
- example of a list within arbitrary host settings:
-  centos9-64m{hostname=static1\\,disks=[8,16],my-list=[my-value1]}-64a
-   1 CentOS 9 64 bit node with roles = master, node name = static1 and lists:
-      disks:
-         - 8
-         - 16
-       my-list:
-         - my-value1
-   1 CentOS 9 64 bit node with roles = agent and the default hypervisor
+           example of a list within arbitrary host settings:
+            centos9-64m{hostname=static1\\,disks=[8,16],my-list=[my-value1]}-64a
+             1 CentOS 9 64 bit node with roles = master, node name = static1 and lists:
+                disks:
+                   - 8
+                   - 16
+                 my-list:
+                   - my-value1
+             1 CentOS 9 64 bit node with roles = agent and the default hypervisor
 
- Generally, it is expected that beaker-hostgenerator output will be redirected to a file, for example:
-  beaker-hostgenerator centos9-64ma > host.cfg
+           Generally, it is expected that beaker-hostgenerator output will be redirected to a file, for example:
+            beaker-hostgenerator centos9-64ma > host.cfg
 
- This can then be used in a Beaker call instead of a static Beaker config.
+           This can then be used in a Beaker call instead of a static Beaker config.
 
         eos
 
