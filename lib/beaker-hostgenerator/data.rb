voxpupuli/beaker-hostgenerator

commit b7fe509f83284e85875108a48603abdd13ff3434

Add CentOS 9 support

lib/beaker-hostgenerator/data.rb
@@ -304,6 +304,18 @@ module BeakerHostGenerator
             ]
           }
         },
+        'centos9-64' => {
+          :general => {
+            'platform' => 'el-9-x86_64'
+          },
+          :docker => {
+            'image'                 => 'quay.io/centos/centos:stream9',
+            'docker_image_commands' => [
+              'cp /bin/true /sbin/agetty',
+              'dnf install -y crontabs initscripts iproute openssl wget which glibc-langpack-en'
+            ]
+          }
+        },
         # Deprecated
         'cisconx-64' => {
           :general => {

commit 4e7d942f5c80d3e49fd727548748d9bc48eec47c

Install hostname on CentOS 9 Stream in docker
This is not available in the minimal CentOS 8 Stream containers and
specinfra relies on this for the hostname -f command to determine the
FQDN.

Fixes: b7fe509f83284e85875108a48603abdd13ff3434

lib/beaker-hostgenerator/data.rb
@@ -334,7 +334,7 @@ module BeakerHostGenerator
             'image'                 => 'quay.io/centos/centos:stream9',
             'docker_image_commands' => [
               'cp /bin/true /sbin/agetty',
-              'dnf install -y crontabs initscripts iproute openssl wget which glibc-langpack-en',
+              'dnf install -y crontabs initscripts iproute openssl wget which glibc-langpack-en hostname',
             ],
           },
         },