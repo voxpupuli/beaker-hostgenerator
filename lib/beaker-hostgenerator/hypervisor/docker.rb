c885f060b5a11ff1f8ee64b841c88f4d08f37142
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2023-07-14 20:23:47 +0200

    Factor out helpers in Docker node generation
    This creates helpers for the image and image commands. That makes it
    easier to expand it in the future without creating one monster method

--- lib/beaker-hostgenerator/hypervisor/docker.rb
+++ lib/beaker-hostgenerator/hypervisor/docker.rb
@@ -9,33 +9,50 @@ module BeakerHostGenerator
 
       def generate_node(node_info, base_config, bhg_version)
         base_config['docker_cmd'] = ['/sbin/init']
-        base_config['image'] = node_info['ostype'].sub(/(\d)/, ':\1')
-        case node_info['ostype']
-        when /^oracle/
-          base_config['image'].sub!(/\w+/, 'oraclelinux')
-        when /^opensuse/
-          base_config['image'].sub!(/(\w+)/, '\1/leap')
-        when /^ubuntu/
-          base_config['image'].sub!(/(\d{2})/, '\1.')
-        when /^rocky/
-          base_config['image'].sub!(/(\w+)/, 'rockylinux')
-        when /^alma/
-          base_config['image'].sub!(/(\w+)/, 'almalinux')
+
+        ostype = node_info['ostype']
+
+        if (commands = image_commands(ostype))
+          base_config['docker_image_commands'] = commands
         end
 
-        base_config['image'] = "amd64/#{base_config['image']}" if node_info['bits'] == '64'
+        base_config['image'] = image(ostype)
+        base_config['image'].prepend('amd64/') if node_info['bits'] == '64'
 
-        case node_info['ostype']
+        base_generate_node(node_info, base_config, bhg_version, :docker)
+      end
+
+      private
+
+      def image(ostype)
+        image = ostype.sub(/(\d)/, ':\1')
+
+        case ostype
+        when /^oracle/
+          image.sub!(/\w+/, 'oraclelinux')
+        when /^opensuse/
+          image.sub!(/(\w+)/, '\1/leap')
         when /^ubuntu/
-          base_config['docker_image_commands'] = [
+          image.sub!(/(\d{2})/, '\1.')
+        when /^rocky/
+          image.sub!(/(\w+)/, 'rockylinux')
+        when /^alma/
+          image.sub!(/(\w+)/, 'almalinux')
+        end
+
+        image
+      end
+
+      def image_commands(ostype)
+        case ostype
+        when /^ubuntu/
+          [
             'cp /bin/true /sbin/agetty',
             'apt-get install -y net-tools wget locales iproute2 gnupg',
             'locale-gen en_US.UTF-8',
             'echo LANG=en_US.UTF-8 > /etc/default/locale',
           ]
         end
-
-        base_generate_node(node_info, base_config, bhg_version, :docker)
       end
     end
   end
