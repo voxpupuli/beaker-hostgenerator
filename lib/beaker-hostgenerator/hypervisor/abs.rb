3f2bf48936d993cbb97c1b4a1697cf5faa1428a8
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2023-10-03 11:25:12 +0200

    Use the safe operator to avoid crashing
    When passing an unknown hostspec the platform is unknown. Using the safe
    operator avoids calling nil.gsub

--- lib/beaker-hostgenerator/hypervisor/abs.rb
+++ lib/beaker-hostgenerator/hypervisor/abs.rb
@@ -24,7 +24,7 @@ module BeakerHostGenerator
 
         case node_info['ostype']
         when /^(almalinux|centos|oracle|redhat|rocky|scientific)/
-          base_config['template'] ||= base_config['platform'].gsub(/^el/, ::Regexp.last_match(1))
+          base_config['template'] ||= base_config['platform']&.gsub(/^el/, ::Regexp.last_match(1))
         when /^amazon/, /^fedora/, /^opensuse/, /^panos/
           base_config['template'] ||= base_config['platform']
         when /^(debian|ubuntu)/
