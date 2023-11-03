5a3566a7949b9f1d344b4b163bca9f1a06babc18
Evgeni Golov <evgeni@golov.de>
2023-11-03 10:26:29 +0100

    explicitly install cronie on EL8+
    cronie is a weak dependency of crontabs, and while CentOS and RHEL install weak dependencies by default, other EL systems do not (so much for compatibility, huh?).
    
    install cronie explicitly, to avoid "Could not find a suitable provider for cron" errors on those EL systems.

--- lib/beaker-hostgenerator/hypervisor/docker.rb
+++ lib/beaker-hostgenerator/hypervisor/docker.rb
@@ -77,7 +77,7 @@ module BeakerHostGenerator
 
       def el_package_install_command(version)
         if version >= 8
-          'dnf install -y crontabs initscripts iproute openssl wget which glibc-langpack-en hostname'
+          'dnf install -y cronie crontabs initscripts iproute openssl wget which glibc-langpack-en hostname'
         else
           'yum install -y crontabs initscripts iproute openssl wget which sysvinit-tools tar ss'
         end
