e1e016f715db4d793dad7aa3a7a5d0e2b45280f3
Steven Pritchard <steven.pritchard@gmail.com>
2026-01-27 11:23:28 -0600

    Default to using bento boxes for vagrant hypervisor
    Replace the default `generic` boxes with equivalent `bento` boxes for the vagrant hypervisor.
    
    Fixes #447

--- lib/beaker-hostgenerator/hypervisor/vagrant.rb
+++ lib/beaker-hostgenerator/hypervisor/vagrant.rb
@@ -15,14 +15,58 @@ module BeakerHostGenerator
         '10' => 'buster',
       }.freeze
 
+      # Map ostype OS names to their bento box equivalents
+      OSTYPE_TO_BENTO_OS_NAME = {
+        'rocky' => 'rockylinux',
+        'oracle' => 'oraclelinux',
+        'redhat' => 'rhel',
+        'amazon' => 'amazonlinux',
+        'osx' => 'macos',
+        'opensuse' => 'opensuse-leap',
+      }.freeze
+
+      # Transforms an ostype like "ubuntu2204" to a bento box name like "bento/ubuntu-22.04"
+      def ostype_to_bento_box(ostype)
+        # Extract the OS name and version from ostype (e.g., "ubuntu2204" -> "ubuntu" and "2204")
+        if ostype =~ /^([a-z]+)(\d+)$/
+          os_name = Regexp.last_match(1)
+          version = Regexp.last_match(2)
+
+          # Map ostype OS name to bento OS name
+          bento_os_name = OSTYPE_TO_BENTO_OS_NAME.fetch(os_name, os_name)
+
+          # Format version with dots for Ubuntu's 4-digit versions (e.g., ubuntu2204 -> 22.04)
+          # Ubuntu uses YY.MM format like 22.04, 24.04
+          formatted_version = if os_name == 'ubuntu' && version.length == 4
+                                "#{version[0, 2]}.#{version[2, 2]}"
+                              else
+                                version
+                              end
+
+          "bento/#{bento_os_name}-#{formatted_version}"
+        else
+          # Fallback to generic if pattern doesn't match
+          "generic/#{ostype}"
+        end
+      end
+
       def generate_node(node_info, base_config, bhg_version)
-        base_config['box'] = case node_info['ostype']
+        ostype = node_info['ostype']
+
+        base_config['box'] = case ostype
                              when /^almalinux/
-                               node_info['ostype'].sub(/(\d)/, '/\1')
+                               ostype.sub(/(\d)/, '/\1')
                              when /^fedora/
-                               node_info['ostype'].sub(/(\d)/, '/\1') + '-cloud-base'
+                               ostype.sub(/(\d)/, '/\1') + '-cloud-base'
+                             when /^centos(\d+)/
+                               version = Regexp.last_match(1)
+                               if version.to_i >= 8
+                                 "centos/stream#{version}"
+                               else
+                                 "centos/#{version}"
+                               end
                              else
-                               "generic/#{node_info['ostype']}"
+                               ostype_to_bento_box(ostype)
                              end
 
         case node_info['platform']
@@ -34,10 +78,7 @@ module BeakerHostGenerator
         when /^centos(\d+)-64/
           version = Regexp.last_match(1)
           if version.to_i >= 8
-            base_config['box'] = "centos/stream#{version}"
             base_config['box_url'] = "https://cloud.centos.org/centos/#{version}-stream/x86_64/images/CentOS-Stream-Vagrant-#{version}-latest.x86_64.vagrant-libvirt.box"
-          else
-            base_config['box'] = "centos/#{version}"
           end
         end
 
