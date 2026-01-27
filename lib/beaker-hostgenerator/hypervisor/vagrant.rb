voxpupuli/beaker-hostgenerator

commit 25d02a056e52e6b135ad93bde8bb30381aef7c37

Generate CentOS Vagrant boxes dynamically
This avoids hardcoding the data in the data file and instead moves it
into the hypervisor.

lib/beaker-hostgenerator/hypervisor/vagrant.rb
@@ -17,7 +17,7 @@ module BeakerHostGenerator
 
       def generate_node(node_info, base_config, bhg_version)
         base_config['box'] = case node_info['ostype']
-                             when /^centos/, /^almalinux/
+                             when /^almalinux/
                                node_info['ostype'].sub(/(\d)/, '/\1')
                              when /^fedora/
                                node_info['ostype'].sub(/(\d)/, '/\1') + '-cloud-base'
@@ -30,6 +30,14 @@ module BeakerHostGenerator
           version = Regexp.last_match(1)
           if (codename = DEBIAN_VERSION_CODES[version])
             base_config['box'] = "debian/#{codename}64"
+          end
+        when /^centos(\d+)-64/
+          version = Regexp.last_match(1)
+          if version.to_i >= 8
+            base_config['box'] = "centos/stream#{version}"
+            base_config['box_url'] = "https://cloud.centos.org/centos/#{version}-stream/x86_64/images/CentOS-Stream-Vagrant-#{version}-latest.x86_64.vagrant-libvirt.box"
+          else
+            base_config = "centos/#{version}"
           end
         end
 

lib/beaker-hostgenerator/data.rb
@@ -142,27 +142,15 @@ module BeakerHostGenerator
                         general: {
                           'platform' => 'el-8-x86_64',
                         },
-                        vagrant: {
-                          'box' => 'centos/stream8',
-                          'box_url' => 'https://cloud.centos.org/centos/8-stream/x86_64/images/CentOS-Stream-Vagrant-8-latest.x86_64.vagrant-libvirt.box',
-                        },
                       },
                       'centos9-64' => {
                         general: {
                           'platform' => 'el-9-x86_64',
                         },
-                        vagrant: {
-                          'box' => 'centos/stream9',
-                          'box_url' => 'https://cloud.centos.org/centos/9-stream/x86_64/images/CentOS-Stream-Vagrant-9-latest.x86_64.vagrant-libvirt.box',
-                        },
                       },
                       'centos10-64' => {
                         general: {
                           'platform' => 'el-10-x86_64',
-                        },
-                        vagrant: {
-                          'box' => 'centos/stream10',
-                          'box_url' => 'https://cloud.centos.org/centos/10-stream/x86_64/images/CentOS-Stream-Vagrant-10-latest.x86_64.vagrant-libvirt.box',
                         },
                       },
                       'panos61-64' => {

commit 71bf775492779360ce6ae2ae2a8bfbd0f6c6c587

Correct non-stream CentOS boxes
Fixes: 25d02a056e52 ("Generate CentOS Vagrant boxes dynamically")

lib/beaker-hostgenerator/hypervisor/vagrant.rb
@@ -37,7 +37,7 @@ module BeakerHostGenerator
             base_config['box'] = "centos/stream#{version}"
             base_config['box_url'] = "https://cloud.centos.org/centos/#{version}-stream/x86_64/images/CentOS-Stream-Vagrant-#{version}-latest.x86_64.vagrant-libvirt.box"
           else
-            base_config = "centos/#{version}"
+            base_config['box'] = "centos/#{version}"
           end
         end
 